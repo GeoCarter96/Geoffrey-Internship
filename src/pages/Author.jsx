b630e1b420843a54f75cfbf33a063437adbeeeee
GeoCarter96 <geolcarter21@gmail.com>
2025-12-29 23:06:57 -0500

    author

--- src/pages/Author.jsx
+++ src/pages/Author.jsx
@@ -1,37 +1,38 @@
 import React, {useState, useEffect} from "react";
 import AuthorBanner from "../images/author_banner.jpg";
 import AuthorItems from "../components/author/AuthorItems";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import AuthorImage from "../images/author_thumbnail.jpg";
 import axios from "axios";
+import AuthorSkeleton from "./AuthorSkeleton";
+import WOW from "wowjs";
+import { useCallback } from "react";
 
 const Author = () => {
-   const [info, setInfo] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState('')
+  const [info, setInfo] = useState("");
+  const [following, setFollowing] = useState(false)
+  const authorId = useParams().id;
      
-     useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('https://us-central1-nft-cloud-functions.cloudfunctions.net/authors?author=73855012');
-        setInfo(response.data.some_array_property); 
-        console.log(response.data)
-      } catch (err){
-        setError(err.name ==='failed to fetch data');
-      }finally{
-        setLoading(false);
-      }  
-    };
-    fetchData();},[]);
-  
-            
-  if (error) return <h2>{error}</h2>
-  
-  return (
-     <div>
+     const getInfo = useCallback(async () => {
+    const response = await axios.get(
+      `https://us-central1-nft-cloud-functions.cloudfunctions.net/authors?author=${authorId}`
+    );
+
+    setInfo(response.data); 
+  }, [authorId]);
+
+  useEffect(() => {
+    new WOW.WOW({
+      live: false,
+    }).init();
+
+   
+    getInfo();
+  }, [getInfo]);
+ 
+return (
     <div id="wrapper">
-      
-      <div  className="no-bottom no-top" id="content">
+      <div className="no-bottom no-top" id="content">
         <div id="top"></div>
 
         <section
@@ -41,72 +42,94 @@ const Author = () => {
           data-bgimage="url(images/author_banner.jpg) top"
           style={{ background: `url(${AuthorBanner}) top` }}
         ></section>
-        {info?.map(info => (
-       
-        <section  key={info.id} aria-label="section">
+
+        <section aria-label="section">
           <div className="container">
             <div className="row">
               <div className="col-md-12">
-               
-                <div  className="d_profile de-flex">
-                  
-                  <div  className="de-flex-col">
-                    
+                <div className="d_profile de-flex">
+                  <div className="de-flex-col">
                     <div className="profile_avatar">
-                      
-                      <img src={info.authorImage} alt="" />
-                      
+                      {info ? (
+                      <img src={info?.authorImage} alt="" />
+                      ) : (
+                        <AuthorSkeleton width="150px" height="150px" borderRadius="50%"/>)}
                       <i className="fa fa-check"></i>
                       <div className="profile_name">
+                       { info ? (
                         <h4>
-                          {info.authorName}
-                          <span className="profile_username">@monicaaaa</span>
+                          {info?.authorName}
+                          <span className="profile_username">{info?.tag}</span>
+                          @{info?.tag}
                           <span id="wallet" className="profile_wallet">
-                            {info.address}
+                            {info?.address}
                           </span>
                           <button id="btn_copy" title="Copy Text">
                             Copy
                           </button>
                         </h4>
-                      </div>
-                      
+                       ) : (
+                        <h4>
+                          <AuthorSkeleton width="200px"/>
+                          <span className="profile_username">
+                            <AuthorSkeleton width="100px"/>
+                            </span>
+                            <span id="wallet" className="profile_wallet">
+                            <AuthorSkeleton width="250px"/>
+                            </span>
+                            </h4>
+                       )}
+                       </div>
                     </div>
-                     
                   </div>
-                 
                   <div className="profile_follow de-flex">
                     <div className="de-flex-col">
-                      <div className="profile_follower">{ info.followers}</div>
-                      <Link to="#" className="btn-main">
-                        Follow
-                      </Link>
+                      {info ? (
+                        <>
+                          <div className="profile_follower">
+                            {info.followers + (following ? 1 : 0)}{" "}
+                            followers
+                          </div>
+                          {following ? (
+                            <Link
+                              to="#"
+                              className="btn-main"
+                              onClick={() => setFollowing(!following)}
+                            >
+                              Unfollow
+                            </Link>
+                          ) : (
+                            <Link
+                              to="#"
+                              className="btn-main"
+                              onClick={() => setFollowing(!following)}
+                            >
+                              Follow
+                            </Link>
+                          )}
+                        </>
+                      ) : (
+                        <div className="profile_follower">
+                          <AuthorSkeleton width="150px" height="40px" />
+                        </div>
+                      )}
                     </div>
-                    </div>
-                  
                   </div>
-                  
                 </div>
-                
               </div>
-              
+
               <div className="col-md-12">
                 <div className="de_tab tab_simple">
-                  <AuthorItems />
+                  <AuthorItems apiData={info} />
                 </div>
               </div>
             </div>
-        
-         
+          </div>
         </section>
-         ))}
-           </div>
-          
       </div>
-      
-     
     </div>
-    
   );
 };
+              
 
 export default Author;
